93ef50687c9df6a657659d635d75d3ca2092c528
Jyotshna <98551142+dev-jyotshna@users.noreply.github.com>
2023-06-08 17:13:54 +0530

    Update queue.java

--- queue.java
+++ queue.java
@@ -1,8 +1,8 @@
 public class Queue <T> implements Iterable <<T> {
-  private java.util.LinkedList <T> list = new java.util.LinkedList <T> () ;
+  private java.util.LinkedList <T> list = new java.util.LinkedList <T> () ; // java implementation of Doubly Linked List
   public Queue() {}
   public Queue (T firstElem ) {
-    pffer(firstElem);
+    enqueue(firstElem); //Offer = Enqueue
   }
   //Return the size of the queue
   public int size() {
@@ -12,3 +12,26 @@ public class Queue <T> implements Iterable <<T> {
   public boolean isEmpty() {
     return size() == 0;
   }
+  //Peek the element at the front of the queue
+  //The method throws an error if the queue is empty
+  public T peek() {
+    if(isEmpty())
+      throw new RuntimeException("Queue Empty");
+    return list.peekFirst();
+  }
+  //Poll an element from the front of the queue
+  //The method throws an error if the queue is empty
+  public T poll() {
+    if(isEmpty())
+      throw new RuntimeException("Queue Empty");
+    return list.removeFirst();
+  }
+  //Add an element to the back of the queue
+  public void enqueue(T elem) {
+    list.addLast(elem);
+  }
+  //Return an iterator to allow the user to traverse through the elements foound inside the queue
+  @Override pulbic java.util.Iterator <T> iterator () {
+    return list.iterator();
+  }
+}

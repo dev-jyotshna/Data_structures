1d13620489f3b72f2da84da86cb1beb6b322936e
Jyotshna <98551142+dev-jyotshna@users.noreply.github.com>
2023-06-23 20:05:07 +0530

    Update HashTableQuadraticProbing.java

--- HashTable/HashTableQuadraticProbing.java
+++ HashTable/HashTableQuadraticProbing.java
@@ -96,3 +96,67 @@ public class HashTableQuadraticProbing <K,V> implements Iterable <K> {
       //The current slot was previously deleted
       if( keyTable[i] == TOMBSTONE) {
         if( j == -1 ) j = i;
+      }
+      //The current cell already contains a key
+      else if( keyTable[i] != null) {
+        //The key we're trying to insert already exists in the hash-table,
+        // so , update its value with the most recent value
+        if( keyTable[i].equals(key)) {
+
+           V oldValue = valueTable[i];
+           if( j == -1) {
+             valueTable[i] = val;
+           }
+           else{
+             keyTable[i] = TOMBSTONE;
+             valueTable[i] = null;
+             keyTable[j] = key;
+             valueTable[j] = val;
+           }
+           modificationCount++;
+           return oldValue;
+        }
+      }
+      //Current ceel is null so an insertion/update can occur
+      else{
+        //No previously encountered deleted buckets
+        if( j == -1) { //No tombstones found yet
+          usedBuckets++; keyCount++;
+          keyTable[i] = key;
+          valueTable[i] = val;
+        }
+        //Previously see deleted bucket. Instead of inserting 
+        //the new element at i where thenull element is insert it where the deleted token was found.
+        else  {
+          keyCount++;
+          keyTable[j] = key;
+          valueTable[j] = val;
+        }
+        modificationCount++;
+        return null;
+      }
+      i = normalizeIndex(hash + P(x++));
+    }while(true);
+  }
+  // Returns true/false on whether a given key exists in the hash table 
+  public boolean containskey(K key) {
+    return hashKey(key);
+  }
+
+  // Returns true/false on whether a given key exists in the hash table used in containskey
+  public boolean hashKey( K key) {
+    get(key);
+    return containsFlag;
+  }
+  
+  //Get teh value associated with the input key.
+  //NOTE: returns null if the value is null AND also returns null if the key does not exists.
+  public V get(K key) {
+    if( key == null) throw new IllegalArgumentException(" Null Key" );
+    final int hash = normalizeIndex(key.hashCode());
+    int i = hash, j = -1, x = 1;
+    //Starting at the original hash index quadratically probe until we find a spot where our key is 
+    //or we hit a null element in which case our element does not exists.
+
+    do { 
+      //Ignore deleted cells, 

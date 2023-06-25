21bd42efd0e5440118a2512c4a8576ce29c67f2e
Jyotshna <98551142+dev-jyotshna@users.noreply.github.com>
2023-06-25 21:38:09 +0530

    Update FenwickTree.java

--- FenwickTree.java
+++ FenwickTree.java
@@ -1,2 +1,19 @@
 //Fenwick Tree( Binary Indexed Tree)
-import java.util.*;
+public class FenwickTree {
+  //This array conatains the Fenwick Tree ranges
+  private long[] tree;
+
+  //Create an empty Fenwick Tree 
+  public FenwickTree( int sz) {
+    tree = new long[sz + 1];
+  }
+
+  //Make sure the 'values' array is one-based meaning
+  //values[0] does not get used, O(n) construction
+  public FenwickTree(long[] values) {
+    if( values == null) 
+      throw new IllegalArgumentException( " Values array cannot be null! ");
+
+    //Make a clone of the values array since we manipulate the array
+    //in place destroying all its original content.
+    this.tree = values.clone();

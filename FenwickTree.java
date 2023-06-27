be264884c05e52953c7c96693423358f4e592692
Jyotshna <98551142+dev-jyotshna@users.noreply.github.com>
2023-06-28 01:00:17 +0530

    Update FenwickTree.java

--- FenwickTree.java
+++ FenwickTree.java
@@ -18,5 +18,21 @@ public class FenwickTree {
     //in place destroying all its original content.
     this.tree = values.clone();
 
-jyotshna hsgusnge
-  mfhdkkms
+    for( int i = 1; i < tree.length; i++) {
+      int j = i + lsb(i);
+      if( j < tree.length) tree[j] += tree[i];
+    }
+  }
+
+// Returns the value of the least significant bit (LSB) 
+//lsb(108) = lsb(0b1101100) = 0b100 = 4
+//lsb(104) = lsb(0b1101000) = 0b1000 = 8
+//lsb(96) = lsb(0b1100000)  = 0b100000 = 32
+//lsb(64) = lsb(0b1000000)  = 0b1000000 = 64
+ private int lsb( int i ) {
+
+  //Isolates the lowest one bit value
+  return i & -i;
+   //An alternative method is to use the Java's built-in method
+   //return Integer.lowestOneBit(i);
+ }
